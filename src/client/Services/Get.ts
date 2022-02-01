35e5c5d18d48c2f3f6dfcd7478b8331b8c03d6c5
rsi-na <75724806+rsi-na@users.noreply.github.com>
2022-02-01 11:54:01 +0100

    commit

---
+++ src/client/Services/Get.ts
@@ -0,0 +1,19 @@
+export default async function Get(
+    url: string,
+    headers:Record<string, unknown> = {}
+): Promise<any>{
+    try {
+        const response: Response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                ...headers
+            },
+        });
+        return response.json();
+    } catch (error) {
+        const err : any = error;
+        throw new Error(err);
+    }
+}
